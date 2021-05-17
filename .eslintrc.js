d8c386acb591a284db1f383864f70a9375b8327d
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2021-05-17 22:19:22 +0300

    Add ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+      "no-unused-vars": "off",
+      "no-case-declarations": "off"
+    }
+};
